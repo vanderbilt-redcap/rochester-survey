97330598d2a38a0f985a7f653531c8c2fd185f6d
Carl Reed <carl.w.reed@vumc.org>
2019-07-12 08:27:21 -0500

    REDCap module boilerplate

---
+++ RochesterSurvey.php
@@ -0,0 +1,8 @@
+<?php
+namespace Vanderbilt\RochesterSurvey;
+
+class RochesterSurvey extends \ExternalModules\AbstractExternalModule {
+	function redcap_survey_page($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id = NULL, $repeat_instance = 1) {
+		
+	}
+}
