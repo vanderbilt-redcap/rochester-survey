27f9abe29d78aba1d24cc2d416a8f135fa16884a
Carl Reed <carlreedw@gmail.com>
2019-07-18 19:04:42 -0500

    implementing field-by-field survey iteration

--- js/survey.js
+++ js/survey.js
@@ -2,16 +2,7 @@
 $('head').append('<link rel="stylesheet" type="text/css" href="CSS_URL">');
 
 // load dashboard content
-$(function() {
-	// append bootstrap js to document
-	// let s = document.createElement("script");
-    // s.type = "text/javascript";
-    // s.src = "BOOTSTRAP_URL";
-    // $("head").append(s);
-	
-	// add dashboard html
-	// $("body").append(SURVEY_HTML);
-	
+$(function() {	
 	Rochester.init();
 });
 
@@ -38,24 +29,15 @@ Rochester.init = function() {
 	$("body").on('click', "#survey-navigation button:last-child", Rochester.nextClicked);
 	
 	// hide most of #pagecontent (except surveytitlelogo and instructions)
-	$("#pagecontent").children().hide();
-	$("#surveytitlelogo").show();
-	$("#surveyinstructions").show();
-	
-	$("#survey-navigation button:eq(0)").hide();
+	$("#pagecontent form").addClass("unseen");
+	$("#survey-navigation button:eq(0)").addClass("unseen");
 	
 	Rochester.surveyTarget = $("#surveytitlelogo")[0];
 }
 
 Rochester.isRealField = function(fieldRow) {
 	// this function returns false if questiontable row is blank descriptive, has display: none, or sq_id == "{}"
-	console.log(fieldRow);
-	console.log($(fieldRow));
-	console.log(/^\s+$/.test($(fieldRow).find("td:eq(1)").html()));
-	console.log($(fieldRow).css("display") == "none");
-	console.log($(fieldRow).attr("sq_id") == "{}");
-	console.log($(fieldRow).hasClass("surveysubmit"));
-	if (!/^\s+$/.test($(fieldRow).find("td:eq(1)").html()) || $(fieldRow).css("display") == "none" || $(fieldRow).attr("sq_id") == "{}" || $(fieldRow).hasClass("surveysubmit")) {
+	if (/^\s+$/.test($(fieldRow).find("td:eq(1)").html()) || $(fieldRow).css("display") == "none" || $(fieldRow).attr("sq_id") == "{}" || $(fieldRow).hasClass("surveysubmit")) {
 		return false;
 	}
 	return true;
@@ -65,59 +47,68 @@ Rochester.openOptions = function() {
 	
 }
 
-Rochester.getFieldInfo = function(fieldRow) {
-	let info = {};
-	info.questionNumber = $(fieldRow).children(".questionnum").text();
-	let fieldName = $(fieldRow).attr('id').replace("-tr", "");
-	info.questionText = $("#label-" + fieldName).text(); 
-	console.log(info);
-}
-
 Rochester.endSurvey = function() {
 	console.log('ending survey');
 }
 
 Rochester.backClicked = function() {
+	let foundNewTarget = false;
+	// try to find a suitable previous questiontable tbody tr to display
+	$(Rochester.surveyTarget).prevAll().each(function(i, e) {
+		if (Rochester.isRealField(e)) {
+			$(Rochester.surveyTarget).addClass("unseen");
+			Rochester.surveyTarget = e;
+			$(e).removeClass("unseen");
+			foundNewTarget = true;
+			return false;
+		}
+	});
 	
+	// if we can't find one, unseen the form and surveytarget, show survey instructions, hide back button
+	if (!foundNewTarget) {
+		// hide
+		$(Rochester.surveyTarget).addClass("unseen");
+		$("#pagecontent form").addClass("unseen");
+		$("#survey-navigation button:eq(0)").addClass("unseen");
+		
+		// show
+		Rochester.surveyTarget = $("#surveytitlelogo")[0];
+		$("#surveytitlelogo").removeClass("unseen");
+		$("#surveyinstructions").removeClass("unseen");
+	}
 }
 
 Rochester.nextClicked = function() {
-	console.log(Rochester.surveyTarget);
 	if (Rochester.surveyTarget == $("#surveytitlelogo")[0]) {
 		// handle case where we're not showing survey contents yet, just survey instructions (e.g., after initialization)
-		console.log("survey target is title logo");
-		$("#surveytitlelogo").hide();
-		$("#surveyinstructions").hide();
+		$("#surveytitlelogo").addClass("unseen");
+		$("#surveyinstructions").addClass("unseen");
+		$("#pagecontent form").removeClass("unseen");
 		
-		$("#pagecontent form").show();
-		$("#questiontable tbody").children().hide();
+		$("#questiontable tbody").children().addClass("unseen");
 		
 		$("#questiontable tbody").children().each(function(i, e) {
-			if (i < 5) {
-				// console.log(e);
-			} else {
-				return false;
-			}
 			if (Rochester.isRealField(e)) {
-				console.log("found real field: " + e);
 				Rochester.surveyTarget = e;
-				$(e).show();
+				$(e).removeClass("unseen");
 				return false;
 			}
 		});
 	} else {
-		console.log (Rochester.surveyTarget + "\n not equal to \n" + $("#surveytitlelogo"));
-	}
-	
-	if (Rochester.hasOwnProperty("targetQuestionRow")) {
-		Rochester.targetQuestionRow = Rochester.targetQuestionRow.next("tr:not(.surveysubmit)");
-		if (Rochester.targetQuestionRow.length == 0) {
+		let foundNewTarget = false;
+		$(Rochester.surveyTarget).nextAll().each(function(i, e) {
+			if (Rochester.isRealField(e)) {
+				$(Rochester.surveyTarget).addClass("unseen");
+				Rochester.surveyTarget = e;
+				$(e).removeClass("unseen");
+				foundNewTarget = true;
+				return false;
+			}
+		});
+		
+		if (!foundNewTarget) {
+			// end survey
 			Rochester.endSurvey();
-		} else {
-			console.log(Rochester.targetQuestionRow);
 		}
-	} else {
-		// Rochester.targetQuestionRow = $("#questiontable > tbody > tr:first-child:not(.surveysubmit)");
-		// console.log(Rochester.targetQuestionRow);
 	}
 }
