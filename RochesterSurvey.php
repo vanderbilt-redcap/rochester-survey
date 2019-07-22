62babf17a2bb42c9f0dac545f27795e9c7d0d1a1
Carl Reed <carl.w.reed@vumc.org>
2019-07-22 13:33:38 -0500

    remove data dictionary

--- RochesterSurvey.php
+++ RochesterSurvey.php
@@ -4,9 +4,9 @@ namespace Vanderbilt\RochesterSurvey;
 class RochesterSurvey extends \ExternalModules\AbstractExternalModule {
 	function redcap_survey_page($project_id, $record = NULL, $instrument, $event_id, $group_id = NULL, $survey_hash, $response_id = NULL, $repeat_instance = 1) {
 		// inject html, js, and css that overrides existing page container
-		$pid = $project_id;
-		$data_dictionary_json = json_encode(\Metadata::getDataDictionary("json", true, array(), array(), false, false, null, $pid));
-		$data_dictionary_json = trim($data_dictionary_json, "\"");
+		// $pid = $project_id;
+		// $data_dictionary_json = json_encode(\Metadata::getDataDictionary("json", true, array(), array(), false, false, null, $pid));
+		// $data_dictionary_json = trim($data_dictionary_json, "\"");
 		// $data_dictionary_json = str_replace(array("\\", "\\\\"), "", $data_dictionary_json);
 		
 		$survey_script = file_get_contents($this->getUrl("js/survey.js"));
