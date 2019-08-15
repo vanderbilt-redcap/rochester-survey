a646feb91d3bab23b01dfb710eee552eadceeefa
Carl Reed <carl.w.reed@vumc.org>
2019-08-15 13:50:08 -0500

    survey.js addition

---
+++ survey_ajax.php
@@ -0,0 +1,10 @@
+<?php
+$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+if (!empty($_POST['action'])) {
+	\REDCap::logEvent("Rochester Survey module - " . $_POST['action'], $_POST['message'], null, null, null, $_GET["pid"]);
+	return json_encode([
+		'action' => $_POST['action'],
+		'message' => $_POST['message']
+	]);
+}
+?>
