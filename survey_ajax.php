17feb2302118a86e184102e7acf6218e4857da87
Carl Reed <carlreedw@gmail.com>
2019-09-19 10:16:16 -0500

    Switched to EM logging

--- survey_ajax.php
+++ survey_ajax.php
@@ -1,11 +1,13 @@
 <?php
 $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 if (!empty($_POST['action'])) {
-	// $module->framework->log("Rochester Survey module - " . $_POST['action'], $_POST['message'], null, null, null, $_GET["pid"]);
-	\REDCap::logEvent("Rochester Survey module - " . $_POST['action'], $_POST['message'], null, null, null, $_GET["pid"]);
-	return json_encode([
-		'action' => $_POST['action'],
-		'message' => $_POST['message']
+	$module->framework->log($_POST['message'], [
+		"action" => $_POST['action']
 	]);
+	// \REDCap::logEvent("Rochester Survey module - " . $_POST['action'], $_POST['message'], null, null, null, $_GET["pid"]);
+	// return json_encode([
+		// 'action' => $_POST['action'],
+		// 'message' => $_POST['message']
+	// ]);
 }
 ?>
