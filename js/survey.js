cdf99e123c1be705461df25c57d53785c57da5a7
Carl Reed <carl.w.reed@vumc.org>
2019-09-20 09:50:51 -0500

    removed signer modal footer with close button

--- js/survey.js
+++ js/survey.js
@@ -623,9 +623,6 @@ Rochester.openSignerModal = function() {
 	html += '\
 					</div>\
 				</div>\
-				<div class="modal-footer">\
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-				</div>\
 			</div>\
 		</div>\
 	</div>';
