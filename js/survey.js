2824f8aee62080b40cdd82fe3b0b432d9ed55b80
Karen Palacios <karenmpalacios@gmail.com>
2023-05-17 09:41:20 -0400

    Update bootstrap attributes

--- js/survey.js
+++ js/survey.js
@@ -729,7 +729,7 @@ Rochester.getOptionsModalHtml = function() {
 					<div class="modal-content">\
 						<div class="modal-header">\
 							<h5 class="modal-title" id="optionsModalLabel">Survey Options</h5>\
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+							<button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">\
 								<span aria-hidden="true">&times;</span>\
 							</button>\
 						</div>\
